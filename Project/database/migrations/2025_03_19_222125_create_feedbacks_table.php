a953c580ff3a585b9099c022d2b0056c0fac3f52
Marzieh <marziehsafari2002@gmail.com>
2025-03-25 00:32:46 +0330

    database

--- Project/database/migrations/2025_03_19_222125_create_feedbacks_table.php
+++ Project/database/migrations/2025_03_19_222125_create_feedbacks_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
 {
     Schema::create('feedbacks', function (Blueprint $table) {
         $table->id();
-        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->foreignId('student_id')->constrained()->onDelete('cascade');
         $table->foreignId('professor_id')->constrained()->onDelete('cascade');
         $table->integer('rating');
         $table->text('comment')->nullable();
