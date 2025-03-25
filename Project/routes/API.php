77d2615e44128d37e6b1f5208429882df5a6c17d
Marzieh <marziehsafari2002@gmail.com>
2025-03-25 23:32:53 +0330

    API

--- Project/routes/API.php
+++ Project/routes/API.php
@@ -12,13 +12,13 @@ use App\Http\Controllers\AdminSystemController;
 Route::get('/api/guest/professors', [ProfessorController::class, 'guestIndex']);
 // نمایش اطلاعات یک استاد مشخص برای مهمان
 Route::get('/api/guest/professors/{id}', [ProfessorController::class, 'guestShow']);
+Route::get('/professors', [ProfessorController::class, 'index']);
 Route::get('/api/courses', [CourseController::class, 'index']); // لیست دوره‌ها برای کاربران
 Route::get('/api/courses/{course}', [CourseController::class, 'show']); // جزئیات یک دوره خاص
 Route::get('/api/guest/courses', [CourseController::class, 'guestIndex']); // لیست دوره‌ها برای مهمان‌ها
 Route::resource('courses', CourseController::class);
 Route::apiResource('students', StudentController::class);
-Route::apiResource('admins', AdminSystemController::class);
-Route::get('/professors', [ProfessorController::class, 'index']); 
+Route::apiResource('admins', AdminSystemController::class); 
 Route::resource('feedback', FeedbackController::class);  
 Route::resource('course', CourseController::class);  
 Route::resource('professor', ProfessorController::class);  
