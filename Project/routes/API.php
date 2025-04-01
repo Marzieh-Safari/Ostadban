c54610a3d24a24db8889b62ae53c71c182295c67
Marzieh <marziehsafari2002@gmail.com>
2025-04-01 23:53:34 +0330

    repair(app)"API's #3

--- Project/routes/API.php
+++ Project/routes/API.php
@@ -35,14 +35,14 @@ Route::prefix('student')->group(function () {
 
 
 
-Route::get('/professor/most-searched', [ProfessorController::class, 'mostSearched']);
+Route::get('/api/professor/most-searched', [ProfessorController::class, 'mostSearched']);
 Route::get('/api/search', [CourseController::class, 'searchAll']);
 Route::get('/api/feedback', [FeedbackController::class, 'guestIndex']);
 //Route::get('/course/guest', [CourseController::class, 'guestIndex']);
 Route::get('/api/course/{id}', [CourseController::class, 'guestShow']);
 Route::get('/api/professor', [ProfessorController::class, 'guestIndex']);// نمایش لیست اساتید برای مهمان
 Route::get('/api/professor/{id}', [ProfessorController::class, 'guestShow']);// نمایش اطلاعات یک استاد مشخص برای مهمان
-Route::get('/professor', [ProfessorController::class, 'index']);
+//Route::get('/professor', [ProfessorController::class, 'index']);
 //Route::get('/api/course', [CourseController::class, 'index']); // لیست دوره‌ها برای کاربران
 //Route::get('/api/course/{course}', [CourseController::class, 'show']); // جزئیات یک دوره خاص
 Route::get('/api/course', [CourseController::class, 'guestIndex']); // لیست دوره‌ها برای مهمان‌ها
@@ -51,4 +51,4 @@ Route::apiResource('student', StudentController::class);
 Route::apiResource('admin', AdminSystemController::class); 
 Route::resource('feedback', FeedbackController::class);  
 Route::resource('professor', ProfessorController::class); 
-Route::get('/professor', [ProfessorController::class, 'index'])->name('professor.index'); 
+//Route::get('/professor', [ProfessorController::class, 'index'])->name('professor.index'); 
